4a58bede602bb5bdb107a03465557690829fc328
7y1o <tdesu.webdesign@gmail.com>
2021-11-06 03:28:26 +0300

    Changes in main script

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import TRSRenderer from './engine/renderer';
+import TRSRenderCollection from './engine/render_collection';
+import TRSUIHBox from './engine/ui/ui_h_box';
+import TRSUIRoundedRect from './engine/ui/ui_rounded_rect';
+import TRSUIVBox from './engine/ui/ui_v_box';
 import './style.css'
 
 // Get app block
@@ -8,6 +12,34 @@ const app = document.querySelector<HTMLDivElement>('#app')!;
 const canvas = document.createElement('canvas');
 app.appendChild(canvas);
 
-// Game engine renderer
+// Run game
 const renderer = new TRSRenderer(canvas);
-renderer.tick(0);
+
+// Rects
+const rect1 = new TRSUIRoundedRect('rect 1', 0, 0, 64, 64, '#ff0000', 'white');
+const rect2 = new TRSUIRoundedRect('rect 2', 0, 0, 64, 64, '#00ff00', 'white');
+const rect3 = new TRSUIRoundedRect('rect 3', 0, 0, 64, 64, '#0000ff', 'white');
+
+rect1.round = 4;
+rect2.round = 8;
+rect3.round = 16;
+
+// Hor box
+const hbox = new TRSUIHBox('hbox', 16, 16, 64, 64, 'black', 'white');
+hbox.gap = 16;
+hbox.addChild(rect1);
+hbox.addChild(rect2);
+hbox.addChild(rect3);
+
+// Ver box
+const vbox = new TRSUIVBox('vbox', 16, 16, 32, 32, 'black', 'white');
+vbox.gap = 16;
+vbox.addChild(hbox);
+vbox.addChild(hbox);
+vbox.addChild(hbox);
+
+renderer.scene(new TRSRenderCollection(
+  'main scene',
+  vbox
+));
+renderer.start();
